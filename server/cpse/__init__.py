d5fa6a72fdf0c91538e164cd3bb66f4785dcbb13
Olivier Chapeau <ochapeau@protonmail.com>
2019-12-07 20:21:47 +0100

    Added main webapp page

--- server/cpse/__init__.py
+++ server/cpse/__init__.py
@@ -1,7 +1,7 @@
 from datetime import datetime
-import os, io, base64, pickle, re, json
+import os, base64, pickle
 
-from flask import Flask, request
+from flask import Flask, request, render_template
 from flask_redis import FlaskRedis
 
 redis_client = FlaskRedis()
@@ -31,14 +31,47 @@ def create_app(test_config=None):
         os.makedirs(app.instance_path)
     except OSError:
         pass
+    
+    ### People ###
+    @app.route('/reset_avg')
+    def reset_avg():
+        avg_people = int(request.args.get("avg"))
+        total_people = int(request.args.get("total"))
+        redis_client.set("avg_people", avg_people)
+        redis_client.set("total_people", total_people)
+        app.logger.info("avg_people setted to %d", avg_people)
+        app.logger.info("total_people setted to %d", total_people)
+        return 'OK'
 
-    @app.route('/')
-    def hello():
-        # redis_client.set("n_people", 1)
-        # n_people = redis_client.get("n_people")
-        # print(n_people)
-        return 'Hello, World!'
+    def get_avg_people():
+        avg_people = redis_client.get("avg_people")
+        # total_people = request.args.get('total')
+        print("Avg People: {}".format(avg_people))
+        return avg
 
+    # def update_avg(n_people):
+    #     avg_people = get_avg_people()
+
+
+    #     return avg
+
+    @app.route('/people', methods=['POST'])
+    def set_people():
+        print(request.is_json)
+        if request.is_json:
+            data = request.get_json()
+            n_people = int(data['n_people'])
+            redis_client.set("n_people", n_people)
+            app.logger.info('n_people setted to %d', n_people)
+        return 'OK'
+
+    # @app.route('/getpeople')
+    def get_people():
+        n_people = int(redis_client.get("n_people"))
+        print("People: {}".format(n_people))
+        return n_people
+
+    ### Decibels ###
     @app.route('/decibels', methods=['POST'])
     def set_decibels():
         print(request.is_json)
@@ -48,16 +81,30 @@ def create_app(test_config=None):
             ts = datetime.now().timestamp()
             tup_db = (ts, decibels)
             redis_client.lpush('db_values', pickle.dumps(tup_db))
-        return 'Setted tuple Redis'
+            app.logger.info('%d decibels sent for ts %d', decibels, ts)
+        return 'OK'
 
-    @app.route('/getdecibels')
+    # @app.route('/getdb')
     def get_decibels():
+        decibels = []
+        tuples = redis_client.lrange('db_values', 0, 50)
+        tuples.reverse()
+        for t in tuples:
+            obj = pickle.loads(t)
+            dt_object = datetime.fromtimestamp(obj[0])
+            print("Date: {}\tdb: {}".format(dt_object, obj[1]))
+            decibels.append({"ts": dt_object.strftime("%d/%m/%Y %H:%M:%S"), "db": obj[1]})
+        return decibels
+
+    @app.route('/pop')
+    def pip():
         db_values = redis_client.lpop('db_values')
         if (db_values is not None):
             obj = pickle.loads(db_values)
             print(obj)
-        return 'Getted tuple Redis'
+        return 'OK'
 
+    ### Receiving Image ###
     @app.route('/img', methods=['POST'])
     def get_img():
         global total_img
@@ -67,27 +114,45 @@ def create_app(test_config=None):
         print(request.mimetype)
         print(request.content_length)
         print(request.is_json)
-
         if request.is_json:
             data = request.get_json()
             total_count = int(data['count'])
             count = total_count
             total_img = ""
-            print("Total Count: {}\t Count: {}".format(total_count, count))
+            # print("Total Count: {}\t Count: {}".format(total_count, count))
+            app.logger.info("Total Count: %d\tCount: {}".format(total_count, count))
         else:
             # print(request.data)
             print("Data length: {}\tContent length: {}".format(len(request.data), request.content_length))
             total_img += request.data.decode()
             count -= 1
-            print("Count: {}".format(count))
+            # print("Count: {}".format(count))
+            app.logger.info("Count: %d".format(count))
             if (count == 0):
                 image_64_decode = base64.b64decode(total_img)
                 img_name = datetime.now().strftime("pics/pic_%d_%m_%Y_%H_%M_%S") + ".jpg"
                 with open(img_name, "wb") as f:
                     f.write(image_64_decode)
-                print("Image \"{}\" saved!".format(img_name))
+                app.logger.info("Image %s saved!".format(img_name))
                 count = total_count
                 total_img = ""
         return 'OK'
+
+    ### Main ###
+    @app.route('/')
+    def index():
+        # redis_client.set("n_people", 1)
+        # n_people = redis_client.get("n_people")
+        # print(n_people)
+        people = {"number": get_people()}
+        decibels = get_decibels()
+        return render_template('index.html', people=people, decibels=decibels)
+
+    @app.route('/hello')
+    def hello():
+        # redis_client.set("n_people", 1)
+        # n_people = redis_client.get("n_people")
+        # print(n_people)
+        return 'Hello, World!'
 
     return app
---
+++ server/cpse/static/chart.js
@@ -0,0 +1,97 @@
+(function () {
+    'use strict'
+  
+    feather.replace()
+
+    // let timeFormat = 'MM/DD/YYYY HH:mm';
+
+    let time_list=[]
+    let db_list=[]
+
+    for (let entry of decibels) {
+      db_list.push(entry['db'])
+      time_list.push(entry['ts'])
+    }
+
+    let db_labels = [
+      {"db": 20, "label":"Mosquito"},
+      {"db": 30, "label":"Quiet rrom"},
+      {"db": 60, "label":"Chatting"},
+      {"db": 70, "label":"Near a motorway"},
+      {"db": 80, "label":"Busy crossroads"},
+      {"db": 90, "label":"Truck, Motorcycle"},
+      {"db": 100, "label":"Subway"},
+      {"db": 110, "label":"Car horn"},
+      {"db": 120, "label":"Aircraft at take off"},
+    ]
+  
+    // Graphs
+    var ctx = document.getElementById('myChart')
+    // eslint-disable-next-line no-unused-vars
+    var myChart = new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: time_list,
+        // [
+        //   'Sunday',
+        //   'Monday',
+        //   'Tuesday',
+        //   'Wednesday',
+        //   'Thursday',
+        //   'Friday',
+        //   'Saturday'
+        // ],
+        datasets: [{
+          data: db_list,
+          // [
+          //   15339,
+          //   21345,
+          //   18483,
+          //   24003,
+          //   23489,
+          //   24092,
+          //   12034
+          // ],
+          lineTension: 0,
+          backgroundColor: 'transparent',
+          borderColor: '#007bff',
+          borderWidth: 4,
+          pointBackgroundColor: '#007bff'
+        }]
+      },
+      options: {
+        scales: {
+					// xAxes: [{
+					// 	type: 'time',
+					// 	time: {
+					// 		parser: timeFormat,
+					// 		// round: 'day'
+					// 		tooltipFormat: 'll HH:mm'
+					// 	},
+					// 	scaleLabel: {
+					// 		display: true,
+					// 		labelString: 'Date'
+					// 	}
+					// }],
+          yAxes: [{
+            ticks: {
+              beginAtZero: true,
+              // Add the decibel label to the Y axis
+              callback: function(value, index, values) {
+                for (let entry of db_labels) {
+                  if (entry.db === value)
+                    return value + ' ' + entry['label']
+                }
+                return value;
+            }
+            },
+            display: true,
+            type: 'logarithmic'
+          }]
+        },
+        legend: {
+          display: false
+        }
+      }
+    })
+  }())
