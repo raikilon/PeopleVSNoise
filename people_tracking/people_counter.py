85b1dbc3c9c31d71a610b30814d37f70dddc1883
raikilon <nolimanzoni94@gmail.com>
2019-12-10 12:41:40 +0100

    tracking app update

--- people_tracking/people_counter.py
+++ people_tracking/people_counter.py
@@ -8,11 +8,10 @@ from models.keras_ssd300 import ssd_300
 from keras_loss_function.keras_ssd_loss import SSDLoss
 
 import numpy as np
-import dlib
 import cv2
 import os
 import requests
-
+import time
 
 url = "http://192.168.43.75:5000/people"
 send_data = False
@@ -76,7 +75,7 @@ H = img_height
 # instantiate our centroid tracker, then initialize a list to store
 # each of our dlib correlation trackers, followed by a dictionary to
 # map each unique object ID to a TrackableObject
-ct = CentroidTracker(maxDisappeared=40, maxDistance=2000)
+ct = CentroidTracker(maxDisappeared=1, maxDistance=2000)
 trackers = []
 trackableObjects = {}
 
@@ -88,7 +87,6 @@ totalUp = 0
 input_images = []
 rgb = None
 frame = None
-clean_frame = None
 img = []
 # loop over frames from the video stream
 while True:
@@ -97,38 +95,19 @@ while True:
     img_path = ''
 
     elems = [(fromdir + f, gt(fromdir + f)) for f in os.listdir(fromdir) if f != '.DS_Store']
-    if len(elems) != 0:
-        img_path = min(elems)[0]
-        # We'll only load one image in this example.
-        input_images = []
-        print(img_path)
-        frame = cv2.imread(img_path)
-        frame = cv2.resize(frame, (img_height, img_width))
-        clean_frame = frame.copy()
-        rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        img = image.img_to_array(image.load_img(img_path, target_size=(img_height, img_width)))
-        os.remove(img_path)
-    else:
-        frame = clean_frame.copy()
-    # img = image.img_to_array(frame)
-    # input_images.append(img)
-    # input_images = np.array(input_images)
 
-    # if we are supposed to be writing a video to disk, initialize
-    # the writer
-
-    # initialize the current status along with our list of bounding
-    # box rectangles returned by either (1) our object detector or
-    # (2) the correlation trackers
-    status = "Waiting"
     rects = []
 
     # check to see if we should run a more computationally expensive
     # object detection method to aid our tracker
-    if img_path != '':
-        # set the status and initialize our new set of object trackers
-        status = "Detecting"
-        trackers = []
+    if len(elems) != 0:
+        img_path = min(elems)[0]
+        print(img_path)
+        input_images = []
+        frame = cv2.imread(img_path)
+        frame = cv2.resize(frame, (img_height, img_width))
+        img = image.img_to_array(image.load_img(img_path, target_size=(img_height, img_width)))
+        os.remove(img_path)
 
         input_images.append(img)
         input_images = np.array(input_images)
@@ -145,7 +124,7 @@ while True:
         for box in y_pred_thresh[0]:
             if box[0] == 15:
                 persons.append(box)
-
+        print(persons)
         for box in persons:
             xmin = box[2]
             ymin = box[3]
@@ -154,132 +133,107 @@ while True:
             # construct a dlib rectangle object from the bounding
             # box coordinates and then start the dlib correlation
             # tracker
-            tracker = dlib.correlation_tracker()
+            # tracker = dlib.correlation_tracker()
 
-            rect = dlib.rectangle(int(xmin), int(ymin), int(xmax), int(ymax))
+            # rect = dlib.rectangle(int(xmin), int(ymin), int(xmax), int(ymax))
             # left, top, right, bottom
-            # rects.append((int(xmin), int(ymin), int(xmax), int(ymax)))
+            rects.append((int(xmin), int(ymin), int(xmax), int(ymax)))
 
-            tracker.start_track(rgb, rect)
+            # tracker.start_track(rgb, rect)
 
             # add the tracker to our list of trackers so we can
             # utilize it during skip frames
-            trackers.append(tracker)
+            # trackers.append(tracker)
 
+        # draw a horizontal line in the center of the frame -- once an
+        # object crosses this line we will determine whether they were
+        # moving 'up' or 'down'
+        cv2.line(frame, (0, int(H // 2)), (W, int(H // 2)), (0, 255, 255), 2)
 
-    # otherwise, we should utilize our object *trackers* rather than
-    # object *detectors* to obtain a higher frame processing throughput
-    else:
-        # loop over the trackers
-        for tracker in trackers:
-            # set the status of our system to be 'tracking' rather
-            # than 'waiting' or 'detecting'
-            status = "Tracking"
+        # use the centroid tracker to associate the (1) old object
+        # centroids with (2) the newly computed object centroids
+        objects = ct.update(rects)
 
-            # update the tracker and grab the updated position
-            tracker.update(rgb)
-            pos = tracker.get_position()
-            pos = tracker.get_position()
-            # unpack the position object
-            startX = int(pos.left())
-            startY = int(pos.top())
-            endX = int(pos.right())
-            endY = int(pos.bottom())
+        # loop over the tracked objects
+        for (objectID, centroid) in objects.items():
+            # check to see if a trackable object exists for the current
+            # object ID
+            to = trackableObjects.get(objectID, None)
 
-            # add the bounding box coordinates to the rectangles list
-            rects.append((startX, startY, endX, endY))
+            # if there is no existing trackable object, create one
+            if to is None:
+                to = TrackableObject(objectID, centroid)
 
-    # draw a horizontal line in the center of the frame -- once an
-    # object crosses this line we will determine whether they were
-    # moving 'up' or 'down'
-    cv2.line(frame, (0, int(H // 2)), (W, int(H // 2)), (0, 255, 255), 2)
+            # otherwise, there is a trackable object so we can utilize it
+            # to determine direction
+            else:
+                # the difference between the y-coordinate of the *current*
+                # centroid and the mean of *previous* centroids will tell
+                # us in which direction the object is moving (negative for
+                # 'up' and positive for 'down')
+                y = [c[1] for c in to.centroids]
+                direction = centroid[1] - np.mean(y)
+                if -4 < direction < 4: direction = 0
+                to.centroids.append(centroid)
 
-    # use the centroid tracker to associate the (1) old object
-    # centroids with (2) the newly computed object centroids
-    objects = ct.update(rects)
+                # check to see if the object has been counted or not
+                if not to.counted:
+                    # if the direction is negative (indicating the object
+                    # is moving up) AND the centroid is above the center
+                    # line, count the object
+                    if direction < 0 and centroid[1] < int(H // 2):
+                        totalUp += 1
+                        to.counted = True
+                        send_data = True
 
-    # loop over the tracked objects
-    for (objectID, centroid) in objects.items():
-        # check to see if a trackable object exists for the current
-        # object ID
-        to = trackableObjects.get(objectID, None)
+                    # if the direction is positive (indicating the object
+                    # is moving down) AND the centroid is below the
+                    # center line, count the object
+                    elif direction > 0 and centroid[1] > int(H // 2):
+                        totalDown += 1
+                        to.counted = True
+                        send_data = True
 
-        # if there is no existing trackable object, create one
-        if to is None:
-            to = TrackableObject(objectID, centroid)
+            # store the trackable object in our dictionary
+            trackableObjects[objectID] = to
 
-        # otherwise, there is a trackable object so we can utilize it
-        # to determine direction
-        else:
-            # the difference between the y-coordinate of the *current*
-            # centroid and the mean of *previous* centroids will tell
-            # us in which direction the object is moving (negative for
-            # 'up' and positive for 'down')
-            y = [c[1] for c in to.centroids]
-            direction = centroid[1] - np.mean(y)
-            if(-4 < direction < 4): direction = 0
-            to.centroids.append(centroid)
+            # draw both the ID of the object and the centroid of the
+            # object on the output frame
+            text = "ID {}".format(objectID)
+            cv2.putText(frame, text, (centroid[0] - 10, centroid[1] - 10),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+            cv2.circle(frame, (centroid[0], centroid[1]), 4, (0, 255, 0), -1)
 
-            # check to see if the object has been counted or not
-            if not to.counted:
-                # if the direction is negative (indicating the object
-                # is moving up) AND the centroid is above the center
-                # line, count the object
-                if direction < 0 and centroid[1] < int(H // 2):
-                    totalUp += 1
-                    to.counted = True
-                    send_data = True
+        # construct a tuple of information we will be displaying on the
+        # frame
+        info = [
+            ("Up", totalUp),
+            ("Down", totalDown),
+        ]
 
-                # if the direction is positive (indicating the object
-                # is moving down) AND the centroid is below the
-                # center line, count the object
-                elif direction > 0 and centroid[1] > int(H // 2):
-                    totalDown += 1
-                    to.counted = True
-                    send_data = True
+        # loop over the info tuples and draw them on our frame
+        for (i, (k, v)) in enumerate(info):
+            text = "{}: {}".format(k, v)
+            cv2.putText(frame, text, (10, H - ((i * 20) + 20)),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.4, (0, 255, 0), 1)
 
-               
+        if send_data:
+            myobj = {"n_people": max(totalDown - totalUp, 0)}
+            x = requests.post(url, json=myobj)
+            send_data = False
 
-        # store the trackable object in our dictionary
-        trackableObjects[objectID] = to
-
-        # draw both the ID of the object and the centroid of the
-        # object on the output frame
-        text = "ID {}".format(objectID)
-        cv2.putText(frame, text, (centroid[0] - 10, centroid[1] - 10),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-        cv2.circle(frame, (centroid[0], centroid[1]), 4, (0, 255, 0), -1)
-
-    # construct a tuple of information we will be displaying on the
-    # frame
-    info = [
-        ("Up", totalUp),
-        ("Down", totalDown),
-        ("Status", status),
-    ]
-
-    # loop over the info tuples and draw them on our frame
-    for (i, (k, v)) in enumerate(info):
-        text = "{}: {}".format(k, v)
-        cv2.putText(frame, text, (10, H - ((i * 20) + 20)),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.4, (0, 255, 0), 1)
+        # increment the total number of frames processed thus far and
+        # then update the FPS counter
+        totalFrames += 1
 
     # show the output frame
     cv2.imshow("Frame", frame)
     key = cv2.waitKey(1) & 0xFF
-
     # if the `q` key was pressed, break from the loop
     if key == ord("q"):
         break
 
-    if send_data:
-        myobj = {"n_people": max(totalDown-totalUp,0)}
-        x = requests.post(url, json = myobj)
-        send_data = False
-
-    # increment the total number of frames processed thus far and
-    # then update the FPS counter
-    totalFrames += 1
+    #time.sleep(2)
 
 # close any open windows
 cv2.destroyAllWindows()
